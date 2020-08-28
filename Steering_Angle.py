b7d6390a18fb49c4a9eaef32e9d732ab32f3b661
Mohammad Wasil Saleem <31696557+MohammadWasil@users.noreply.github.com>
2020-08-29 00:06:48 +0200

    update steering angle

--- Steering_Angle.py
+++ Steering_Angle.py
@@ -4,10 +4,15 @@ Created on Fri Aug 28 02:18:27 2020
 
 @author: wasil
 """
+
 import keyboard
 
 Steering_angle = 0
 STEERING_FACTOR = 0.05
+MAXIMUM_ANGLE = 45
+
+# W, A, S, D
+keys = [0, 0, 0, 0]
 
 # code: https://stackoverflow.com/questions/24072790/detect-key-press-in-python
 while True:
@@ -16,31 +21,40 @@ while True:
         # If we steer right, angle towrds the right increases.
         if keyboard.is_pressed('D'):
             # To limit the angle between 0 and 90.
-            if(Steering_angle >= 0 and Steering_angle < 90):
+            if(Steering_angle >= 0 and Steering_angle < MAXIMUM_ANGLE):
                 Steering_angle += 1 * STEERING_FACTOR
-            elif Steering_angle > 90:
-                Steeing_angle = 90
-                
-            print(Steering_angle)
+            elif Steering_angle > MAXIMUM_ANGLE:
+                Steeing_angle = MAXIMUM_ANGLE
+
+            #print(Steering_angle)
+            
+            # one zero - Keybiard inouts.
+            print([0, 0, 0, 1, 0])
         
-        # If we steer left, angle towrds the left increases.
+        # If we steer left, angle towrds the right increases.
         elif keyboard.is_pressed('A'):
             
-            # To limit the angle between 0 and -90.
-            if Steering_angle <= 0 and Steering_angle > -90:
+            # To limit the angle between 0 and 90.
+            if Steering_angle <= 0 and Steering_angle > -MAXIMUM_ANGLE:
                 Steering_angle -= 1 * STEERING_FACTOR
-            elif Steering_angle < -90:
-                Steeing_angle = -90
+            elif Steering_angle < -MAXIMUM_ANGLE:
+                Steeing_angle = -MAXIMUM_ANGLE
                 
-            print(Steering_angle)
+            #print(Steering_angle)
             
-        # or else steer angle goes to zero, i.e. going straight.
+            # one zero
+            print([0, 1, 0, 0, 0])
+             
+        # or else steer angle goes to zero.
         else:
             Steering_angle = 0
             #print(Steering_angle)
+            
+            # one zero
+            print([0, 0, 0, 0, 0])
     except:
         pass
+
     
     if keyboard.is_pressed('Q'):
-        break
-        
+        break    
